b6cf1cd6bd85ae4c17ef2c1b9bc230c8ed8b1e24
denizuss <cinardenizkursunlu@hotmail.com>
2026-01-31 18:50:01 +0300

    Add files via upload

---
+++ lua/autorun/luftwaffelfpoveda.lua
@@ -0,0 +1,11 @@
+player_manager.AddValidModel( "SD Unter-Officer", 					"models/SD/SD_NCO.mdl" )
+list.Set( "PlayerOptionsModel",  "SD Unter-Officer", 					"models/SD/SD_NCO.mdl" )
+
+player_manager.AddValidModel( "SD Officer", 					"models/SD/SD_CO.mdl" )
+list.Set( "PlayerOptionsModel",  "SD Officer", 					"models/SD/SD_CO.mdl" )
+
+player_manager.AddValidModel( "SD Officer coat", 					"models/SD/SD_CO_COAT.mdl" )
+list.Set( "PlayerOptionsModel",  "SD Officer coat", 					"models/SD/SD_CO_COAT.mdl" )
+
+player_manager.AddValidModel( "SD Private", 					"models/SD/SD_EN.mdl" )
+list.Set( "PlayerOptionsModel",  "SD Private", 					"models/SD/SD_EN.mdl" )
